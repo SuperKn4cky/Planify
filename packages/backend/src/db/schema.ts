d277386b4a0fc3b807f266197c1ea5ce625a346f
Quentin <superretards22@gmail.com>
2025-10-20 15:53:32 +0200

    add user revocation timestamp

--- packages/backend/src/db/schema.ts
+++ packages/backend/src/db/schema.ts
@@ -1,9 +1,12 @@
-import { pgTable, serial, integer, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-    id: serial("id").primaryKey(),
-    email: varchar("email", { length: 60 }),
-    first_name: varchar("first_name", { length: 30 }),
-    last_name: varchar("last_name", { length: 30 }),
-    password: varchar("password", { length: 255 }),
+    id: serial("id").primaryKey().notNull(),
+    email: varchar("email", { length: 60 }).unique().notNull(),
+    first_name: varchar("first_name", { length: 30 }).notNull(),
+    last_name: varchar("last_name", { length: 30 }).notNull(),
+    password: varchar("password", { length: 255 }).notNull(),
+    revocation_timestamp: timestamp("revocation_timestamp")
+        .defaultNow()
+        .notNull(),
 });
