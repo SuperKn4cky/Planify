207ab5c16c7f6ec3a4b56e7dfc6c045f8670cb9a
Quentin <superretards22@gmail.com>
2025-10-22 18:12:12 +0200

    fix env path

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -23,7 +23,7 @@ export class WebApp {
     constructor() {
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = path.dirname(__filename);
-        dotenv.config({ path: path.resolve(__dirname, '../../../.env.local') });
+        dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
         this.port = parseInt(process.env.PORT || "4000", 10);
         this.databaseUrl = process.env.DATABASE_URL || undefined;
         this.frontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
@@ -41,7 +41,6 @@ export class WebApp {
         if (!this.jwtSecret) {
             this.jwtSecret = generateJwtSecret(this.nodeEnv);
         }
-
         this.db = await startDatabase(this.databaseUrl);
         const userService = new UserService(this.db);
         const userController = new UserController(userService);
