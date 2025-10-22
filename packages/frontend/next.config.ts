d289ce70a9ca44b4dfc3aea55a67fb3ead6d0e40
Quentin <superretards22@gmail.com>
2025-10-22 10:06:05 +0200

    add next config for api rewrite call

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+  },
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
+  async rewrites() {
+    if (!process.env.BACKEND_URL) throw new Error('BACKEND_URL missing');
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.BACKEND_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
