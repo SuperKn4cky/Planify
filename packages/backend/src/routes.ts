251862196f246169f2cd6b78dd4178e643b4e632
Quentin <superretards22@gmail.com>
2025-10-23 14:27:47 +0200

    instantiate controller, service & middleware and move user endpoint to dedicated function and add login route

--- packages/backend/src/routes.ts
+++ packages/backend/src/routes.ts
@@ -1,28 +1,36 @@
 import express, { Request, Response } from "express";
+import { DB } from "./db/drizzle.js";
+import AuthMiddleware from "./middlewares/authMiddleware.js";
+import AuthService from "./services/authService.js";
 import UserController from "./controllers/userController.js";
+import UserService from "./services/userService.js";
 
 export default class Routes {
     private app: express.Application;
-    private userController: UserController;
+    private db: DB;
+    private jwtSecret: string;
 
-    constructor(
-        app: express.Application,
-        controllers: { userController: UserController },
-    ) {
+    private authMiddleware: AuthMiddleware;
+    private authService: AuthService;
+
+    private userController: UserController;
+    private userService: UserService;
+
+    public constructor(app: express.Application, db: DB, jwtSecret: string) {
         this.app = app;
-        this.userController = controllers.userController;
+        this.db = db;
+        this.jwtSecret = jwtSecret;
+        this.authService = new AuthService(this.jwtSecret);
+        this.authMiddleware = new AuthMiddleware(this.authService);
+        this.userService = new UserService(this.db, this.authService);
+        this.userController = new UserController(this.userService);
     }
 
     public async register(): Promise<void> {
-        this.app.post(
-            "/users",
-            this.userController.createUser.bind(this.userController),
-        );
-        this.app.get(
-            "/users/:id",
-            this.userController.getUserByID.bind(this.userController),
-        );
+        // User routes
+        this.userRoutes();
 
+        // Health check endpoint
         this.app.get("/health", (req: Request, res: Response) => {
             res.status(200).json({ status: "OK" });
         });
@@ -34,5 +42,24 @@ export default class Routes {
                 req: req.originalUrl,
             });
         });
+    }
+
+    private async userRoutes(): Promise<void> {
+        this.app.post(
+            "/users",
+            this.userController.createUser.bind(this.userController),
+        );
+
+        this.app.post(
+            "/login",
+            this.userController.loginUser.bind(this.userController),
+        );
+
+        this.app.get(
+            "/users/:id",
+            this.authMiddleware.isAuthenticated.bind(this.authMiddleware),
+            this.authMiddleware.authorizeUserId.bind(this.authMiddleware),
+            this.userController.getUserByID.bind(this.userController),
+        );
     }
 }
