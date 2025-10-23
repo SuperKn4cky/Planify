187d00235f9ee9e3ae84623686840b06c3788fc4
Quentin <superretards22@gmail.com>
2025-10-23 14:31:05 +0200

    add login controller function

--- packages/backend/src/controllers/userController.ts
+++ packages/backend/src/controllers/userController.ts
@@ -2,40 +2,63 @@ import { NextFunction, Request, Response } from "express";
 import { UserWithPassword, User } from "../entities/userEntite.js";
 import AppError from "../middlewares/errorHandler.js";
 import UserService from "../services/userService.js";
-import { DrizzleQueryError } from "drizzle-orm";
 
 export default class UserController {
     private userService: UserService;
 
-    constructor(userService: UserService) {
+    public constructor(userService: UserService) {
         this.userService = userService;
     }
 
-    public async createUser(req: Request, res: Response, next: NextFunction) : Promise<void> {
+    public async createUser(
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> {
         try {
             const newUser = new UserWithPassword(req.body);
-            const row = await this.userService.createUser(newUser);
-            const user = new User(row);
+            const user = await this.userService.createUser(newUser);
             res.json({
                 message: "User created successfully",
                 data: user.toPublic(),
+                token: user.token,
             });
         } catch (error) {
-            if (error instanceof DrizzleQueryError && (error as any).cause.code) {
-                return next(new AppError("This email is already in use.", 400));
-            }
             next(error);
         }
     }
 
-    public async getUserByID(req: Request, res: Response, next: NextFunction) : Promise<void> {
+    public async loginUser(
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> {
+        try {
+            const { email, password } = req.body;
+            if (!email || !password) {
+                throw new AppError("Email and password are required", 400);
+            }
+            const token = await this.userService.loginUser(email, password);
+            res.json({
+                message: "Login successful",
+                token: token,
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    public async getUserByID(
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> {
         try {
             const { id } = req.params;
             if (!id || isNaN(Number(id))) {
                 throw new AppError("Invalid or missing user id", 400);
             }
-            const row = await this.userService.getUserByID(Number(id));
-            const user = new User(row);
+            const user = await this.userService.getUserByID(Number(id));
             res.json(user.toPublic());
         } catch (error) {
             next(error);
