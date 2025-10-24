6f846d27d8a10eecf518f99839a7e697d20ddf8f
Quentin <superretards22@gmail.com>
2025-10-24 20:46:34 +0200

    fix use of setup file

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -2,6 +2,7 @@ export default {
     preset: "ts-jest/presets/default-esm",
     testEnvironment: "node",
     testMatch: ["**/__tests__/**/*.test.ts"],
+    setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
     // This mapping tells Jest to use the .ts/.tsx file when it sees a .js import.
     // It's necessary for ESM projects where TypeScript requires you to use the .js extension in imports.
     moduleNameMapper: {
