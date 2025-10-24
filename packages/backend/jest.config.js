5b92ace5de87806dd15bc8938a20cde55219373a
Quentin <superretards22@gmail.com>
2025-10-24 15:41:26 +0200

    fix jest to work with ESM

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,3 +1,15 @@
-export const preset = "ts-jest";
-export const testEnvironment = "node";
-export const testMatch = ["**/__tests__/**/*.test.ts"];
+export default {
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+    testMatch: ["**/__tests__/**/*.test.ts"],
+    // This mapping tells Jest to use the .ts/.tsx file when it sees a .js import.
+    // It's necessary for ESM projects where TypeScript requires you to use the .js extension in imports.
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+    transform: {
+        // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+        // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+        "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
+    },
+};
