9ff1882073b554976e243da0570332421b1336b4
Quentin <superretards22@gmail.com>
2025-10-29 10:57:49 +0100

    fix drizzle config for new build method of database URL

--- packages/backend/drizzle.config.ts
+++ packages/backend/drizzle.config.ts
@@ -1,11 +1,18 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+const DATABASE_URL =
+    process.env.POSTGRES_USER &&
+    process.env.POSTGRES_PASSWORD &&
+    process.env.POSTGRES_DB
+        ? `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@db:5432/${process.env.POSTGRES_DB}`
+        : undefined;
+
 export default defineConfig({
     out: "./drizzle",
     schema: "./src/db/schema.ts",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: DATABASE_URL!,
     },
 });
