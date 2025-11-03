9132a6b99a6176e205d569a6457a41e6b068fd1d
Quentin <superretards22@gmail.com>
2025-11-03 11:05:18 +0100

    add app frame componant to mask sidebar on auth path

---
+++ packages/frontend/src/components/AppFrame.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Sidebar from "@/components/Sidebar";
+
+export default function AppFrame({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const onAuth = pathname?.startsWith("/auth") ?? false;
+
+  if (onAuth) {
+    return <>{children}</>;
+  }
+
+  return (
+    <div className="min-h-dvh bg-white">
+      <div className="mx-auto flex">
+        <Sidebar />
+        <main className="flex-1 p-8">{children}</main>
+      </div>
+    </div>
+  );
+}
