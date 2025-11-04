842c09cfe86beceac21b678ddc193bf7d18245a8
Quentin <superretards22@gmail.com>
2025-11-04 03:05:12 +0100

    fix text color

--- packages/frontend/src/components/AppFrame.tsx
+++ packages/frontend/src/components/AppFrame.tsx
@@ -14,7 +14,7 @@ export default function AppFrame({ children }: { children: React.ReactNode }) {
         <div className="min-h-dvh bg-white">
             <div className="mx-auto flex">
                 <Sidebar />
-                <main className="flex-1 p-8">{children}</main>
+                <main className="flex-1 p-8 text-[#0F172A]">{children}</main>
             </div>
         </div>
     );
