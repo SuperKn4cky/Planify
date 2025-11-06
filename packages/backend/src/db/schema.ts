eb5c85e8d8fcd01014091a6e6175e6034429a44d
SuperKnacky <superretards22@gmail.com>
2025-11-06 14:17:01 +0100

    add task and folder schema

--- packages/backend/src/db/schema.ts
+++ packages/backend/src/db/schema.ts
@@ -1,4 +1,7 @@
-import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, timestamp, varchar, integer, pgEnum, primaryKey  } from "drizzle-orm/pg-core";
+
+export const statusEnum = pgEnum("status", ["todo", "doing", "done"]);
+export const permissionEnum = pgEnum("permission", ["owner", "read", "write"]);
 
 export const users = pgTable("users", {
     id: serial("id").primaryKey().notNull(),
@@ -8,3 +11,51 @@ export const users = pgTable("users", {
     password: varchar("password", { length: 255 }).notNull(),
     revocation_timestamp: timestamp("revocation_timestamp").notNull(),
 });
+
+export const tasks = pgTable("tasks", {
+    id: serial("id").primaryKey().notNull(),
+    folder_id: integer("folder_id").references(() => folders.id, { onDelete: "set null"}),
+    responsible_user: integer("responsible_user").references(() => users.id, { onDelete: "set null"}),
+    title: varchar("title", { length: 60}).notNull(),
+    description: varchar("description", { length: 255}),
+    due_date: timestamp("due_date"),
+    status: statusEnum("status").notNull().default("todo"),
+    priority: integer("priority").notNull().default(1),
+});
+
+export const users_own_tasks = pgTable(
+    "users_own_tasks",
+    {
+        user_id: integer("user_id")
+            .notNull()
+            .references(() => users.id, { onDelete: "cascade" }),
+        task_id: integer("task_id")
+            .notNull()
+            .references(() => tasks.id, { onDelete: "cascade" }),
+        permission: permissionEnum("permission").notNull().default("owner"),
+    },
+    (t) => [
+        primaryKey({ columns: [t.user_id, t.task_id] }),
+    ],
+);
+
+export const folders = pgTable("folders", {
+    id: serial("id").primaryKey().notNull(),
+    name: varchar("name", {length: 60}).notNull(),
+});
+
+export const users_own_folders = pgTable(
+    "users_own_folders",
+    {
+        user_id: integer("user_id")
+            .notNull()
+            .references(() => users.id, { onDelete: "cascade" }),
+        folder_id: integer("folder_id")
+            .notNull()
+            .references(() => folders.id, { onDelete: "cascade" }),
+        permission: permissionEnum("permission").notNull().default("owner"),
+    },
+    (t) => [
+        primaryKey({ columns: [t.user_id, t.folder_id] }),
+    ],
+);
