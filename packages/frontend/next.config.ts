a4381bbef90daa8edf62a650c93fd4f6e8ae0a77
Quentin <superretards22@gmail.com>
2025-11-10 15:01:17 +0100

    fix backend url for test

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -1,27 +1,28 @@
 import type { NextConfig } from "next";
 
-const BACKEND_INTERNAL_URL =
-  process.env.NODE_ENV === "development"
+export const BACKEND_INTERNAL_URL = ["development", "test"].includes(
+    process.env.NODE_ENV ?? "",
+)
     ? "http://localhost:4000"
     : "http://Planify-back:4000";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${BACKEND_INTERNAL_URL}/:path*`,
-      },
-    ];
-  },
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                hostname: "localhost",
+            },
+        ],
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: `${BACKEND_INTERNAL_URL}/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
